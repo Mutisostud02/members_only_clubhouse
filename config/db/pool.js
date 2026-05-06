3b76515cad3d392ac59fd199e18c16895c5edf8c
Mutisostud02 <mutiso8395@gmail.com>
2026-05-06 16:30:36 +0300

    edited pool.js to add ssl property

--- config/db/pool.js
+++ config/db/pool.js
@@ -2,6 +2,8 @@ const { Pool } = require('pg')
 require('dotenv').config()
 
 module.exports = new Pool({
-    connectionString:process.env.DATABASE_URL,
-   ssl: process.env.NODE_ENV === 'production'
-})
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
